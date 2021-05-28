0e9dfc6c9439b6db7413b4a4848ab8df78856c28
Mumun.sf <82895736+munyaja@users.noreply.github.com>
2021-05-29 05:57:52 +0700

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,16 @@
+#def reverse(s): 
+ #   str = "" 
+  #  for i in s: 
+   #     str = i + str 
+    #return str 
+
+s = input("Input kalimat: ") 
+#print ("String awal : ",end="") 
+print (s) 
+
+print ("String yang sudah dibalik : ",end="") 
+str = "" 
+for i in s: 
+    str = i + str
+print(str)
+#print (reverse(s)) 
